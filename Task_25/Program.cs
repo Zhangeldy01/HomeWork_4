039199899023490210ae440471193e4aa2b73805
Zhangeldy <shakhanov_zhan@mail.ru>
2022-12-14 09:19:55 +0300

    added files

---
+++ Task_25/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+Console.Write("Введите число A: ");
+  int a = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int b = Convert.ToInt32(Console.ReadLine());
+
+int Exponentiation(int a, int b)
+{
+  int result = a;
+  for(int i=1; i < b; i++)
+  {
+    result = result * a;
+  } 
+     // int result = Math.Pow(a, b);
+     return result;
+}
+int exponentiation = Exponentiation(a, b);
+Console.WriteLine($"A в степени B равно: {exponentiation}");
+
+
---
+++ Task_27/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Write("Введите число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+  int SumOfDigits(int numN)
+  {
+    int counter = Convert.ToString(numN).Length;
+    //int advance = 0;
+    //int result = 0;
+    int sum = 0;
+    int num = 0;
+    for (int i = 0; i < counter; i++)
+    {
+        num = numN % 10;
+        sum = sum + num;
+        numN = numN / 10;
+        
+      /* advance = numN - numN % 10;
+      result = result + (numN - advance);
+      numN = numN / 10; */
+    }
+    return sum;
+    // return result;
+  }
+
+int sumOfDig = SumOfDigits(numN);
+Console.WriteLine("Сумма цифр в числе: " + sumOfDig);
